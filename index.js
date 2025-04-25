b2ac2a246bdacca48edf9f81a93caa72a1dc173f
Daniel Källberg <daniel.kallberg@me.com>
2025-04-25 22:44:00 +0200

    Reset Index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+console.log("🚀 index.js är igång!");
+
 import './src/routes/bookings.js';
 import './src/routes/getAvailableSlots.js';
 import './src/routes/status.js';
 import './src/routes/health.js';
-import './src/routes/meetingTypes.js';
