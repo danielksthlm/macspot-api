6fefc13a737f82497914637749c1f59225196d50
Daniel Källberg <daniel.kallberg@me.com>
2025-04-25 22:36:47 +0200

    ny index

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-
-const app = express();
-
-app.use(cors({
-  origin: [
-    'https://klrab.webflow.io',
-    'https://klrab.se'
-  ]
-}));
-
-// Därefter importerar du routes
 import './src/routes/bookings.js';
 import './src/routes/getAvailableSlots.js';
 import './src/routes/status.js';
