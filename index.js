c376b9f3247e62c57239aa73b6c551d4fc0370cb
Daniel Källberg <daniel.kallberg@me.com>
2025-04-25 22:29:49 +0200

    Ny Routes

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ import './src/routes/bookings.js';
 import './src/routes/getAvailableSlots.js';
 import './src/routes/status.js';
 import './src/routes/health.js';
+import './src/routes/meetingTypes.js';
