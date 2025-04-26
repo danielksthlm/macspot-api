99861d4d691f1935d575e6488f620c47b358b89c
Daniel Källberg <daniel.kallberg@me.com>
2025-04-26 08:39:38 +0200

    Route: meetingTypes.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ console.log("🚀 index.js är igång!");
 
 import './src/routes/bookings.js';
 import './src/routes/getAvailableSlots.js';
+import './src/routes/meetingTypes.js';
 import './src/routes/status.js';
 import './src/routes/health.js';
