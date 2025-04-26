f151d69d64d8e9ed93fab81833106d30dc34c3ab
Daniel Källberg <daniel.kallberg@me.com>
2025-04-26 08:46:58 +0200

    Nu index / tom index

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 console.log("🚀 index.js är igång!");
-
-import './src/routes/bookings.js';
-import './src/routes/getAvailableSlots.js';
-import './src/routes/meetingTypes.js';
-import './src/routes/status.js';
-import './src/routes/health.js';
