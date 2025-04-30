a6215558258969f7c995ebc956faaf0769fd332d
Daniel Källberg <daniel.kallberg@me.com>
2025-04-30 16:51:26 +0200

    Update

--- meeting_types/index.js
+++ meeting_types/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { getDb } from '../src/lib/db/db.js';
+import { getDb } from '../../src/lib/db/db.js';
 
 context.log("📂 Nuvarande katalog:", __dirname);
 
