2dfc0210283fd487eecd64cd51f1cd955c38fa04
Daniel Källberg <daniel.kallberg@me.com>
2025-05-01 11:08:28 +0200

    nytt

--- meeting_types/index.js
+++ meeting_types/index.js
@@ -1,18 +1,10 @@
-import { pingShared, add } from '../shared/db.js';
+import { hello } from '../shared/testmodule.js';
 
-context.log("🧪 meeting_types startar");
-
-// context.log(pingShared());
-// context.log("2 + 3 =", add(2, 3));
-
-// const client = await getDb().connect();
-// ...
-// client.release();
-// ...
-// const values = ...
+context.log("🧪 Funktion startad");
+context.log(hello());
 
 context.res = {
   status: 200,
   headers: { 'Content-Type': 'application/json' },
-  body: ["dummy1", "dummy2"]
+  body: ["dummy"]
 };
