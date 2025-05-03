b0b055a677ff16bcc7ee8336492142b485c10e3e
Daniel Källberg <daniel.kallberg@me.com>
2025-05-03 21:24:01 +0200

    nytt

--- shared/db.js
+++ shared/db.js
@@ -1,18 +1,4 @@
-import postgres from 'postgres';
-
-let sql = null;
-
 export function getDb() {
-  if (sql) return sql;
-
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    console.error("❌ DATABASE_URL saknas");
-    return null;
-  }
-
-  console.log("🌐 postgres.js använder:", connectionString);
-
-  sql = postgres(connectionString, { ssl: 'require' }); // krävs för Azure
-  return sql;
+  console.log("✅ Det här är ett dummy-db-anrop.");
+  return "OK";
 }
