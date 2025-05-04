f6b8e410a011d8c30d46437a83c09af32d534404
Daniel Källberg <daniel.kallberg@me.com>
2025-05-04 13:46:52 +0200

    Åter 1

--- meeting_types/index.js
+++ meeting_types/index.js
@@ -1,7 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: parseInt(process.env.PGPORT || '5432', 10),
   ssl: { rejectUnauthorized: false }
 });
 
