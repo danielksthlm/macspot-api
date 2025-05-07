074ebdba6270fb1dfb3961dea27070fcc583b247
Daniel Källberg <daniel.kallberg@me.com>
2025-05-07 15:55:07 +0200

    9

--- getavailableslots/index.js
+++ getavailableslots/index.js
@@ -11,7 +11,7 @@ export default async function (context, req) {
     try {
       context.log("📦 Miljövariabler:");
       context.log("GRAPH_CLIENT_ID", process.env.GRAPH_CLIENT_ID);
-      context.log("GRAPH_CLIENT_SECRET", process.env.GRAPH_CLIENT_SECRET ? "[hemlig]" : "❌ Saknas");
+      context.log("GRAPH_CLIENT_SECRET", process.env.GRAPH_CLIENT_SECRET ? "[hemlig]" : "❌ Saknass");
       context.log("GRAPH_TENANT_ID", process.env.GRAPH_TENANT_ID);
       context.log("GRAPH_USER_ID", process.env.GRAPH_USER_ID);
 
