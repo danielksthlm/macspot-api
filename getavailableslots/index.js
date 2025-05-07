aef0e99fa37ae4c7e37f223944e4846a7c6e624b
Daniel Källberg <daniel.kallberg@me.com>
2025-05-07 08:19:36 +0200

    Test

--- getavailableslots/index.js
+++ getavailableslots/index.js
@@ -1,19 +1,10 @@
 module.exports = async function (context, req) {
-  context.log('✅ Funktion anropad');
-
-  if (!req || !req.body) {
-    context.res = {
-      status: 400,
-      body: { error: 'Saknar req.body' }
-    };
-    return;
-  }
-
-  const { email, meeting_type, meeting_length } = req.body;
-  context.log('📥 Data mottagen:', { email, meeting_type, meeting_length });
+  context.log('✅ Funktion getavailableslots anropad');
 
   context.res = {
     status: 200,
-    body: { message: 'Funktion kördes utan fel', input: req.body }
+    body: {
+      message: 'Testfunktion körd korrekt'
+    }
   };
 };
