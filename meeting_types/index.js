45dd279bd4c70a7856cb30e704dee28d130beb9c
Daniel Källberg <daniel.kallberg@me.com>
2025-05-21 16:07:43 +0200

    Test 82

--- meeting_types/index.js
+++ meeting_types/index.js
@@ -1,13 +1,4 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT || '5432', 10),
-  ssl: { rejectUnauthorized: false }
-});
+const pool = require('../shared/db/pgPool');
 
 module.exports = async function (context, req) {
   const requiredEnv = ['PGUSER', 'PGHOST', 'PGDATABASE', 'PGPASSWORD', 'PGPORT'];
--- validate_contact/index.js
+++ validate_contact/index.js
@@ -1,13 +1,5 @@
-const { Pool } = require('pg');
+const pool = require('../shared/db/pgPool');
 const { v4: uuidv4 } = require('uuid');
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT || '5432', 10),
-  ssl: { rejectUnauthorized: false }
-});
 
 module.exports = async function (context, req) {
   try {
